25af483a9d6a6ad730bd9701ca743840ad8e8d54
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-11-10 11:50:58 +0100

    Added Main and Punto

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Punto up = new Punto(1.4F, 18);
+        Punto left = new Punto(-28.5F, 2.3F);
+
+        System.out.println("The highest point is " + up +
+                "\nThe leftest point is " + left);
+    }
+}
